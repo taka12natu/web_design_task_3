d4e077cefb5603f399540bd4c7ad614dc6af08c9
taka12natu <hvhpdrmcmtr12@gmail.com>
2021-08-26 00:33:11 +0900

    add accordion menu

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
-$(function(){
-    
-    //スマホメニューボタン
-    $("#sp_menu_open").on("click", function(){
-        $(this).fadeOut("normal");
-        $("#sp_menu_close").fadeIn("normal");
-        $("#menu").fadeIn("normal");
-    });
 
-    $("#sp_menu_close").on("click", function(){
-        $(this).fadeOut("normal");
-        $("#sp_menu_open").fadeIn("normal");
-        $("#menu").fadeOut("normal");
-    }); 
 
-    //モーダルウィンドウ
-    $('.js-modal-open').on('click',function(){
-        $('.js-modal').fadeIn();
-        return false;
-    });
-
-    $('.js-modal-close').on('click',function(){
-        $('.js-modal').fadeOut();
-        return false;
-    });
-
-    //メインビジュアル画像切り替え
-    $(".main_img:not(:first-child)").hide();
-    setInterval(function() {
-      $(".main_img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".main_img_box");
-    },5000);
-
+//スマホメニューボタン アコーディオン表示
+$("#sp_menu_open").on("click", function(){
+    $("#menu").slideDown(1000);
+    $(this).fadeOut("normal");
+    $("#sp_menu_close").fadeIn("normal");
 });
+
+$("#sp_menu_close").on("click", function(){
+    $("#menu").slideUp(1000);
+    $(this).fadeOut("normal");
+    $("#sp_menu_open").fadeIn("normal");
+});
+
+//モーダルウィンドウ
+$('.js-modal-open').on('click',function(){
+    $('.js-modal').fadeIn();
+    return false;
+});
+
+$('.js-modal-close').on('click',function(){
+    $('.js-modal').fadeOut();
+    return false;
+});
+
+//メインビジュアル画像切り替え
+$(".main_img:not(:first-child)").hide();
+setInterval(function() {
+    $(".main_img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".main_img_box");
+},5000);
+
 
 //スライドショー
 function sliderSetting(){
     var width = $(window).width();
-    if(width <= 600){
+    if(width < 600){
         $('.slider').not('.slick-initialized').slick({
             mobileFirst: true,
             infinite: true, //スライドのループ
@@ -53,8 +52,22 @@ function sliderSetting(){
 // 初期表示の実行
 sliderSetting();
 
+// spでメニュー開いてからpcにした場合にメニューが非表示になることを防ぐため
+function MenuDisplay(){
+    var width = $(window).width();
+    if(width > 600){
+        if($('#menu').css('display') == 'none') {
+            $('#menu').css('display', 'flex');
+        }
+    }else{
+        if($('#menu').css('display') == 'flex') {
+            $('#menu').css('display', 'none');
+        }
+    }
+}
+
 // リサイズの実行
 $(window).resize( function() {
-sliderSetting();
+    sliderSetting();
+    MenuDisplay();
 });
-
